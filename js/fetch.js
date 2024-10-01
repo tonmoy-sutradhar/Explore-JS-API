d724a2e2abbe8435defde6ba75d7a77cdbf843c3
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-10-01 11:48:13 +0600

    Load btn and fetch data from url practice done.

---
+++ js/fetch.js
@@ -0,0 +1,16 @@
+// console.log("JS file connected");
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+// // fetch(url)
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+function loadData() {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
--- js/json.js
+++ js/json.js
@@ -1,2 +1,33 @@
 const user = { id: 1, name: "Tonmoy sutradhar", job: "Web-Developer" };
-console.log(user);
+// JavaScript Object Notaion (JSON)
+const stingified = JSON.stringify(user);
+// console.log(stingified);
+// console.log(user);
+
+/*
+  OUUPTU ----->>
+  {"id":1,"name":"Tonmoy sutradhar","job":"Web-Developer"} ---->> JSON
+ { id: 1, name: 'Tonmoy sutradhar', job: 'Web-Developer' } ---->> JS
+*/
+
+const shop = {
+  owner: "Tonmoy",
+  address: {
+    street: "khilkhet, vut er goli",
+    city: "Dhaka",
+    country: "Bangladesh",
+  },
+  products: ["Laptop", "mic", "monitor", "keyboard"],
+  revenue: 50000,
+  isOpen: true,
+  isNew: false,
+};
+
+// conver json string
+const stringifield = JSON.stringify(shop);
+// conver json parse
+const jsparse = JSON.parse(stringifield);
+
+console.log(shop);
+console.log(stringifield);
+console.log(jsparse);
